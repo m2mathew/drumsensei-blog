97a7d03a7460da407ecaff3f03f04b0b1bdc7ba8
Mike Mathew <m2mathew@me.com>
2018-11-29 13:20:02 -0800

    Adds link to / on ds logo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,14 @@
 // External Dependencies
 import React from 'react'
 import get from 'lodash/get'
-// import { Link } from 'gatsby'
+import { Link } from 'gatsby'
 
 // Internal Dependencies
 import DsLogo from '../assets/ds-logo.svg';
 import { rhythm, scale } from '../utils/typography'
+
+// Local Variables
+const PrestoDarkPurple = '#280154';
 
 // Component Definition
 class Layout extends React.Component {
@@ -45,7 +48,7 @@ class Layout extends React.Component {
         <h3
           style={{
             color: 'rebeccapurple',
-            // fontFamily: 'Montserrat, sans-serif',
+            fontFamily: 'Montserrat, sans-serif',
             marginTop: 64,
             marginBottom: rhythm(-1),
           }}
@@ -71,8 +74,8 @@ class Layout extends React.Component {
         <nav
           style={{
             background: '#f5f5f5',
-            borderBottom: '4px solid #280154',
-            boxShadow: '3px 0 5px #280154',
+            borderBottom: `4px solid ${PrestoDarkPurple}`,
+            boxShadow: `3px 0 5px ${PrestoDarkPurple}`,
             display: 'flex',
             alignItems: 'center',
             height: 64,
@@ -86,7 +89,16 @@ class Layout extends React.Component {
               transform: 'translateY(8px)',
             }}
           >
-            <DsLogo />
+            <Link
+              to="/"
+              style={{
+                boxShadow: 'none',
+                textDecoration: 'none',
+                color: 'inherit',
+              }}
+            >
+              <DsLogo />
+            </Link>
           </div>
         </nav>
         <main
